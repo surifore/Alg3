99f51a6c88f2f1ce260c2590b27e453a8a1fae41
G34754 Florian Delporte <>
2012-11-22 11:28:17 +0100

    Ajout de classes pour export plus facile

---
+++ CarrefourClientFeuVoiture/src/be/esi/g34754/alg3/carrefour/client/feu/voiture/NewClass1.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.esi.g34754.alg3.carrefour.client.feu.voiture;
+
+/**
+ *
+ * @author g34754
+ */
+public class NewClass1 {
+    
+}
